fb038654b67317b5ca4fbb56f7a9e42ab2981070
PurushShanu <purushottam@rigi.club>
2023-04-05 21:27:04 +0530

    dev done for books

--- models/users.js
+++ models/users.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose');
-const constants = require('../constants/constants');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const constants = require("../constants/constants");
+const uniqueValidator = require("mongoose-unique-validator");
 
-const usersSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email_id: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        required: true,
-        type: String
+      required: true,
+      type: String,
     },
     user_type: {
-        type: String,
-        required: true,
-        enum: [constants.roles]
+      type: String,
+      required: true,
+      enum: [constants.roles],
     },
-    first_name : String,
-    last_name : String,
-    stage_name : String, //in artist only
-    session_price : Number,
-}, {
-    timestamps: true
-});
+    first_name: String,
+    last_name: String,
+    stage_name: String, //in artist only
+    session_price: Number,
+    session_duration: Number, // in mins only
+  },
+  {
+    timestamps: true,
+  }
+);
 
 usersSchema.plugin(uniqueValidator, {
-    message: 'Error: expected {PATH} to be unique.',
-})
-
+  message: "Error: expected {PATH} to be unique.",
+});
 
 const users = mongoose.model("users", usersSchema);
 module.exports = users;
--- routes/router.js
+++ routes/router.js
@@ -1,25 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/user-controller');
-const constants = require('../constants/constants');
-const authController = require('../controllers/AuthController')
-const jobController = require('../controllers/job-controllers')
+const userController = require("../controllers/user-controller");
+const constants = require("../constants/constants");
+const authController = require("../controllers/AuthController");
+const jobController = require("../controllers/job-controllers");
+const bookingController = require("../controllers/booking-controller")();
 
-
-
-router.get('/', (req, res) => {
-    res.send("{message:'Welcome to my site'}");
+router.get("/", (req, res) => {
+  res.send("{message:'Welcome to my site'}");
 });
-router.post('/registration', userController.registration);
-router.post('/login', userController.login);
+router.post("/registration", userController.registration);
+router.post("/login", userController.login);
 
-router.use(authController.authorizeToken);//
+router.use(authController.authorizeToken); //
 
-router.get('/user-profile', userController.getUserProfile);
-router.post('/post-job', authController.restrictTo(constants.roles.recruiter), jobController.postJob)
-router.get('/job/:id', jobController.getJob)
-router.post('/all-job', jobController.getAllJob)
+router.get("/user-profile", userController.getUserProfile);
+router.post(
+  "/post-job",
+  authController.restrictTo(constants.roles.recruiter),
+  jobController.postJob
+);
+router.get("/job/:id", jobController.getJob);
+router.post("/all-job", jobController.getAllJob);
+router.post("/book", bookingController.createBooking);
+router.post(
+  "/book/accept_or_reject",
+  bookingController.acceptOrRejectBooksByArtist
+);
+router.get("/blockedbooks", bookingController.getBooksForArtistAcceptance);
+router.get(
+  "/artist/upcoming_books",
+  bookingController.getUpcomingBooksForArtist
+);
+router.get("/user/upcoming_books", bookingController.getUpcomingBooksForUser);
+router.get(
+  "/artist/completed_books",
+  bookingController.getCompletedBooksForArtist
+);
+router.get("/user/completed_books", bookingController.getCompletedBooksOfUser);
+router.get("/dates", bookingController.getdatesForBookings);
 //router.post('/apply', restrictTo(constants.roles.applicant), jobController.applyJob)
-
 
 module.exports = router;
